2de04a1296780f8fbed00a2f49806f06f7b567f5
Michael Garee <m.garee@gmail.com>
2019-01-04 11:21:28 -0500

    Fixes read_initial_elos ignoring filename arg.

--- util.py
+++ util.py
@@ -16,17 +16,17 @@ class Util:
     def read_games(filename):
         """ Load games history from file. """
         games = [item for item in csv.DictReader(open(filename))]
-        
+
         # apply typecasting to data
         for game in games:
             game['elo_prob1'] = float(game['elo_prob1']) if game['elo_prob1'] != '' else None
             game['result1'] = float(game['result1']) if game['result1'] != '' else None
-            
+
         return games
 
     @staticmethod
     def write_games(games, filename):
-        """ Write games data to file """        
+        """ Write games data to file """
         fieldnames = 'date,season,playoff,team1,team2,result1,elo1,elo2,elo_prob1'.split(',')
 
         with open(filename, 'w', newline='') as csvfile:
@@ -34,12 +34,13 @@ class Util:
             writer.writeheader()
             writer.writerows(map(Util.format_game_entry, games))
 
-        print("Games data saved to " + filename)        
-        
+        print("Games data saved to " + filename)
+
     @staticmethod
     def read_initial_elos(filename="data/initial_elos.csv"):
+        """ Read initial Elo ratings for teams """
         teams = {}
-        for row in [item for item in csv.DictReader(open("data/initial_elos.csv"))]:
+        for row in [item for item in csv.DictReader(open(filename))]:
             teams[row['team']] = {
                 'name': row['team'],
                 'season': None,  # tracker to handle between-season rating reversion
@@ -51,7 +52,7 @@ class Util:
     @staticmethod
     def format_game_entry(game):
         """ Apply text formatting to game entry before writing to file """
-        g = game.copy()
+        g = game.copy()  # pylint: disable=invalid-name
 
         if g['result1'] is not None and g['result1'] != 0.5:
             g['result1'] = int(g['result1'])
@@ -59,4 +60,4 @@ class Util:
         for field in ['elo1', 'elo2', 'elo_prob1']:
             g[field] = "%.5f" % g[field]
 
-        return g        
+        return g
